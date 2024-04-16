1a13bb3c5f80e5582751eab10db8c59d2733f36b
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2024-04-16 07:25:35 -0300

    Implementacion de clase Articulo

---
+++ Articulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    public class Articulo
+    {
+        public int id { get; set; }
+
+        public string codigo { get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public decimal precio { get; set; }
+
+        //public Marca marcaArticulo { get; set; } ---> falta implementar clase Marca
+        
+        //public Categoria categoriaArticulo { get; set; } ---> falta implementar clase Categoria
+
+        List<string> imagenes;
+    }
+}
