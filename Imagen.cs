d96e57f06d4ae8840a8e0f601191a77526937669
MartinAgarcia1710 <118487586+MartinAgarcia1710@users.noreply.github.com>
2024-04-17 19:26:03 -0300

    Clase Imagen
    Queda creada la clase Imagen en una rama nueva.

---
+++ Imagen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    internal class Imagen
+    {
+        public int IDImagen { get; set; }
+        public int IDArticulo { get; set; }
+        public Uri Url { get; set; }
+
+    }
+}
