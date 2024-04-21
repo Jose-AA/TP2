17a0324aac7a7085b5efdfeee55233b13ceb8d18
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2024-04-21 20:40:51 -0300

    arquitectura en capas v2

---
+++ dominio/Articulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int id { get; set; }
+
+        public string codigo { get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public decimal precio { get; set; }
+
+        public Marca marcaArticulo { get; set; } 
+        
+        public Categoria categoriaArticulo { get; set; } 
+
+        List<Imagen> imagenes;
+    }
+}
